c80270cec36a0806c8cae0c022471de39a687dc0
UltraSoundX <haroldxin@foxmail.com>
2020-04-24 12:17:10 +0800

    新增抢座 身份认证

--- CQHTTP/app.py
+++ CQHTTP/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request,jsonify,send_from_directory,make_response,send_file
 import os,sys,json,requests
-from extend import Appoint
+from extend import Appoint,Verify
 import _thread
-import time
+import time,re
 
 预约 = Appoint()
+认证 = Verify()
 app = Flask(__name__)
 
 def sendmsg(userid):
@@ -12,13 +13,13 @@ def sendmsg(userid):
     id = 预约.check(userid)
     print(id)
     if id != 0:
-        msg = "欢迎使用山一医图书馆预约\nQQ已经绑定账号："+str(id) + "\n快速预约请发送：预约\n取消预约请发送：取消预约"
-        url = "http://127.0.0.1:5700/send_private_msg?user_id="+str(userid)+"&message="+str(msg)
+        msg = "欢迎使用山一医图书馆预约\nQQ已经绑定学号："+str(id) + "\n快速预约请发送：预约\n取消预约请发送：取消预约\n绑定QQ请发送：绑定 你的学号\n重新绑定学号请发送：认证"
+        url = "http://lab.radiology.link:5700/send_private_msg?user_id="+str(userid)+"&message="+str(msg)
         requests.get(url)
         return 0
     else :
-        msg = "欢迎使用山一医图书馆预约\n请前往 https://lab.radiology.link/signup.html 进行注册\n 注册后打开 https://lab.radiology.link/ 绑定QQ开始快速预约"
-        url = "http://127.0.0.1:5700/send_private_msg?user_id="+str(userid)+"&message="+str(msg)
+        msg = "欢迎使用山一医图书馆预约\n如果已经注册网页版账号请回复 绑定 学号 进行账号绑定\n请前往 https://lab.radiology.link/signup.html 进行注册\n 注册后打开 https://lab.radiology.link/ 绑定QQ开始快速预约"
+        url = "http://lab.radiology.link:5700/send_private_msg?user_id="+str(userid)+"&message="+str(msg)
         requests.get(url)
         return 0
 
@@ -32,15 +33,72 @@ def receive():
     print (msgtype)
     if msgtype == 'message': 
         msg = recv['message']
-        print (recv['raw_message'])
         if msg == '预约':
             预约.appoint(userid)
             return 'Success'
         if msg == '取消预约':
             预约.cancel(userid)
             return 'Success'
+        if '认证' in msg:
+            if "," in msg or "，" in msg:
+                msg = msg.replace("，",",")
+                raw = msg.replace(" ","")[2:]
+                verify = re.split(",",raw)
+                id = verify[0]
+                password = verify[1]
+                code = verify[2]
+                if 认证.登录(userid,id,password,code):
+                    reply = {
+                        'reply':"认证成功"
+                    }
+                    return jsonify(reply)
+                else:
+                    reply = {
+                        'reply':"认证失败，请重试"
+                    }
+                    return jsonify(reply)
+            认证.sendmsg(userid,"正在获取验证码...")
+            url = "http://api.radiology.link:5700/send_private_msg"
+            jpg = "https://api.radiology.link/Library/img/"+str(认证.验证码(userid))
+            message = "验证码：[CQ:image,file=" + str(jpg) +"]\n请回复 认证 教育在线学号,教育在线密码,验证码 用逗号分隔,看不清请重新回复认证"
+            payload = {"user_id":"951671556","message":message}
+            a = requests.post(url,data=payload)
+            return 'Success'
+        if '抢座' in msg:
+            reply = {
+                    'reply':"抢座执行后无法取消 确定执行请回复\n抢座 确认"
+                }
+            return jsonify(reply)
+            if '确认' in msg:
+                预约.appointnow(userid)
+                return 'Success'
+        if '绑定' in msg:
+            msg = msg.replace(" ","")
+            id = msg[2:]
+            if id == "":
+                id = 预约.check(userid)
+                reply = {
+                    'reply':"请输入 绑定 你的学号\n现在已经绑定的学号是"+id
+                }
+                return jsonify(reply)
+            try:
+                if 认证.绑定(userid,id) :
+                    reply = {
+                        'reply':"学号绑定成功"
+                    }
+                    return jsonify(reply)
+                else :
+                    reply = {
+                        'reply':"学号绑定失败"
+                    }
+                    return jsonify(reply)
+            except:
+                reply = {
+                    'reply':"请检查是否注册\n请前往 https://lab.radiology.link/signup.html 进行注册"
+                }
+                return jsonify(reply)
         reply = {
-            'reply':"指定位置预约请前往 https://lab.radiology.link/ \n发送 预约 至机器人可快速预约\n发送 取消预约 可快速取消"
+            'reply':"指定位置预约请前往 https://lab.radiology.link/ \n发送 预约 至机器人可快速预约\n发送 取消预约 可快速取消\n发送 抢座 可实时抢座\n发送 绑定 可绑定QQ \n发送 认证 进行学号绑定更换"
         }
         return jsonify(reply)
 
--- CQHTTP/extend.py
+++ CQHTTP/extend.py
@@ -1,4 +1,7 @@
-import pymysql,string,random,time,hashlib,requests,datetime
+import pymysql,string,random,time,hashlib,requests,datetime,json
+from io import BytesIO
+from requests.cookies import RequestsCookieJar
+from urllib.parse import quote
 
 try:
     数据库 = pymysql.connect("localhost","Radiology-Library","Radiology-Library","Library")
@@ -10,6 +13,66 @@ except:
     print('\033[33mWeb API:Down')
     print('\033[32m验证接口：Ready')
     print('\033[0m')
+    
+
+class Verify:
+
+    def sendmsg(self,QQ,msg):
+        url = "http://lab.radiology.link:5700/send_private_msg?user_id="+str(QQ)+"&message="+str(msg)
+        requests.get(url)
+        return 0
+
+    def 获取Cookie(self):
+        url = "http://jwc.sdfmu.edu.cn/academic/common/security/login.jsp"
+        Cookie = requests.utils.dict_from_cookiejar(requests.get(url).cookies)
+        return Cookie
+    
+    def 验证码(self,QQ):
+        Cookie = self.获取Cookie()
+        游标 = 数据库.cursor()
+        更新Cookie = 'UPDATE `user` SET `cookie`="%s" WHERE `qq`="%d"' % (str(Cookie),QQ)
+        游标.execute(更新Cookie)
+        url = 'http://jwc.sdfmu.edu.cn/academic/getCaptcha.do'
+        验证码二进制 = requests.get(url,cookies=Cookie).content
+        文件名 = "/www/wwwroot/api/Library/img/"+str(hashlib.md5(验证码二进制).hexdigest())+".jpg"
+        with open(文件名, "wb") as f:
+            f.write(验证码二进制)
+            f.close()
+        文件名 = str(hashlib.md5(验证码二进制).hexdigest())+".jpg"
+        return 文件名
+    
+    def 登录(self,QQ,用户名,密码,验证码):
+        游标 = 数据库.cursor()
+        获取Cookie = "SELECT  `cookie` FROM `user` WHERE `qq`='%s'"%(QQ)
+        游标.execute(获取Cookie)
+        Cookie = 游标.fetchone()[0]
+        Cookie = eval(Cookie)
+        header = {"content-type":"application/x-www-form-urlencoded"}
+        载荷 = "j_username="+str(用户名)+"&j_password="+quote(str(密码))+"&j_captcha="+str(验证码)
+        url = "http://jwc.sdfmu.edu.cn/academic/j_acegi_security_check"
+        请求 = requests.post(url,data=载荷,headers=header,cookies=Cookie)
+        if 请求.url == 'http://jwc.sdfmu.edu.cn/academic/index_new.jsp':
+            self.更改绑定(QQ,用户名)
+            return 1
+        else:
+            return 0
+    
+    def 绑定(self,QQ,id):
+        游标 = 数据库.cursor()
+        绑定学号 = "UPDATE `user` SET `qq`='%s' WHERE `studentid`='%s'" % (QQ,id)
+        if 游标.execute(绑定学号) == 1 :
+            return 1
+        else :
+            return 0
+    
+    def 更改绑定(self,QQ,id):
+        游标 = 数据库.cursor()
+        绑定学号 = "UPDATE `user` SET `studentid`='%s' WHERE `qq`='%s'" % (id,QQ)
+        if 游标.execute(绑定学号):
+            self.sendmsg(QQ,'学号成功更改 回复 绑定 查询当前学号')
+            return 0
+        else :
+            self.sendmsg(QQ,'学号成功更改 回复 绑定 查询当前学号')
 
 class Appoint :
 
@@ -23,7 +86,7 @@ class Appoint :
             return 0
 
     def sendmsg(self,QQ,msg):
-        url = "http://127.0.0.1:5700/send_private_msg?user_id="+str(QQ)+"&message="+str(msg)
+        url = "http://lab.radiology.link:5700/send_private_msg?user_id="+str(QQ)+"&message="+str(msg)
         requests.get(url)
         return 0
 
@@ -34,6 +97,25 @@ class Appoint :
             self.sendmsg(QQ,'预约取消成功!')
         else:
             self.sendmsg(QQ,'暂无可使用的预约')
+            
+    def appointnow(self,QQ):
+        游标 = 数据库.cursor()
+        查询 = "SELECT `studentid` FROM `user` WHERE `qq` = '%s'" % (QQ)
+        总书库 = (4,5,8,9,10,11,14,15)
+        if 游标.execute(查询) == 1:
+            id = 游标.fetchone()[0]
+            area = 总书库[random.randint(0,7)]
+            url = "https://api.radiology.link/Library/RestController.php/?do=now&username="+str(id)+"&area="+str(area)
+            recv = requests.get(url).json()
+            if 'msg' in recv :
+                self.sendmsg(QQ,'预约失败，是否已有预约或者重试一次')
+            if len(recv) == 2 :
+                area = recv['area']
+                seat = recv['seat']
+                msg = '抢座成功，第'+str(area)+'书库，请在30分钟内到达图书馆签到'
+                self.sendmsg(QQ,msg)
+        else :
+            self.sendmsg(QQ,'您是否绑定学号了？回复 绑定 查看绑定的学号')
     
     def appoint(self,QQ):
         游标 = 数据库.cursor()
@@ -51,7 +133,7 @@ class Appoint :
                 位置 = 1333 + seat - 1
                 更新 = "UPDATE `user` SET `seat`='%s',`datepicker`='%s',`area`='%s' WHERE `studentid`='%s'" % (位置,日期,书库,用户)
                 if 游标.execute(更新) == 1:
-                    msg = '预约成功，第'+ str(area+1) + '书库，第' + str(seat) + '号 \n 取消预约请发送 取消预约'
+                    msg = '预约成功，第'+ str(area+1) + '书库，第' + str(seat) + '号 \n取消预约请发送 取消预约'
                     self.sendmsg(QQ,msg)
                 else:
                     msg = '预约失败，请重试一次预约指令或登录网页端进行预约 https://lab.radiology.link'
@@ -62,7 +144,7 @@ class Appoint :
                 位置 = 1389 + seat - 1
                 更新 = "UPDATE `user` SET `seat`='%s',`datepicker`='%s',`area`='%s' WHERE `studentid`='%s'" % (位置,日期,书库,用户)
                 if 游标.execute(更新) == 1:
-                    msg = '预约成功，第'+ str(area+1) + '书库，第' + str(seat) + '号 \n 取消预约请发送 取消预约'
+                    msg = '预约成功，第'+ str(area+1) + '书库，第' + str(seat) + '号 \n取消预约请发送 取消预约'
                     self.sendmsg(QQ,msg)
                 else:
                     msg = '预约失败，请重试一次预约指令或登录网页端进行预约 https://lab.radiology.link'
